2cabd0bff1514686640c58f149136412b17e1b7b
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-06-16 00:47:54 +0800

    commit 21

--- login.php
+++ login.php
@@ -33,9 +33,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Validate credentials
-    if(empty($username_err) && empty($password_err) && empty($email)){
+    if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password, email FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
