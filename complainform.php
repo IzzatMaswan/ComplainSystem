d8ca6aed6320f30df07e64a7145c76a9b7599f73
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-06-16 09:41:46 +0800

    change 1

--- complainform.php
+++ complainform.php
@@ -1,56 +1,3 @@
-<?php 
-#Script handle form
-
-if (strlen($_POST["name"]) >0)
-    { 
-    $_POST["name"] = $_POST["name"];
-    }
-else
-    {
-    $_POST["name"]= null;
-    echo '<p><b> You Forgot to Enter Your Name! </b></p>';
-    }
-
-
-if (strlen($_POST["comments"]) >0) 
-    { 
-    $_POST["comments"] = $_POST["comments"]; 
-    }
-else
-    {
-    $_POST["comments"]= null;
-    echo '<p><b> You Forgot to Enter Your Comments! </b></p>';
-    }
-if(! (strlen($_POST["email"]) >0)) 
-    {
-    $_POST["email"] = null;
-    echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
-    }
-
-
-if (isset($_POST["gender"])) 
-    {
-    if ($_POST["gender"] == 'M') 
-    {
-        $message = '<b><p>Good Day, Sir </b></p>';
-    }
-    if ($_POST["gender"] == 'F') 
-    {
-        $message = '<b><p>Good Day, Madam </b></p>';
-    }
-}else{
-        $_POST["gender"] = null;
-        echo '<p><b> You Forgot to Choose Your Gender! </b></p>';
-    }
-
-
-if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) 
-    { echo "Thank you," .$_POST["name"]. " for the following comments: <br /> <tt>" .
-    $_POST["comments"]. " </tt> <p> We will reply to you at <i>" . $_POST["email"]. 
-    "</i></p>"; 
-    echo $message;
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,6 +7,7 @@ if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"])
     <title>Document</title>
 </head>
 <body>
+        <form action="user.php" method = "post">
         <p><b>Name</b><input type="text" size="12" maxlength="12" name="name"></p>
         <p><b>Email Address:</b> <input type="text" size="12" maxlength="36" name="email"></p>
         <p><b>Gender</b>
